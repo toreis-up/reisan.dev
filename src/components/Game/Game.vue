b886653d6b916858a60a7f1b3e17e71ab3c3981a
とれいす <65478744+toreis-up@users.noreply.github.com>
2023-11-18 23:33:56 +0900

    :sparkles: Chore: configure initial phaser files (#2)
    * :sparkles: Chore: configure initial phaser files
    
    * :wrench: Fix: add ScriptHost lib

---
+++ src/components/Game/Game.vue
@@ -0,0 +1,23 @@
+<template>
+  <Suspense>
+    <phaser-container />
+
+    <template #fallback>
+      <div>
+        loading...
+      </div>
+    </template>
+  </Suspense>
+</template>
+
+<script lang="ts">
+import PhaserContainer from './PhaserContainer.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'game-container',
+  components: {
+    PhaserContainer
+  }
+})
+</script>
---
+++ src/components/Game/phasercore/Player.ts
@@ -0,0 +1,47 @@
+import Phaser from "phaser";
+import { TestScene } from "../Scene/TestScene";
+import { Direction } from "./Direction";
+
+export class Player {
+  constructor(
+    private sprite: Phaser.GameObjects.Sprite,
+    private tilePos: Phaser.Math.Vector2
+  ) {
+    const offsetX = TestScene.TILE_SIZE / 2;
+    const offsetY = TestScene.TILE_SIZE;
+
+    this.sprite.setOrigin(0.5, 1);
+    this.sprite.setPosition(
+      tilePos.x * TestScene.TILE_SIZE + offsetX,
+      tilePos.y * TestScene.TILE_SIZE + offsetY
+    );
+    this.sprite.setFrame(1);
+  }
+
+  getPosition(): Phaser.Math.Vector2 {
+    return this.sprite.getBottomCenter();
+  }
+
+  setPosition(position: Phaser.Math.Vector2): void {
+    this.sprite.setPosition(position.x, position.y);
+  }
+
+  stopAnimation(direction: Direction) {
+    const animationManager = this.sprite.anims.animationManager;
+    const stagingFrame = animationManager.get(direction).frames[1].frame.name;
+    this.sprite.anims.stop();
+    this.sprite.setFrame(stagingFrame);
+  }
+
+  startAnimation(direction: Direction) {
+    this.sprite.anims.play(direction);
+  }
+
+  getTilePos() {
+    return this.tilePos.clone();
+  }
+
+  setTilePos(tilePosition: Phaser.Math.Vector2) {
+    this.tilePos = tilePosition.clone();
+  }
+}
