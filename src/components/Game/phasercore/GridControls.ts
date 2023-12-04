38623cd68e7b7dd915aaaf1f8e43fe5940cd0dde
tores-up <toreis@reisan.dev>
2023-12-04 13:30:35 +0900

    refactor: cutout grid physics control

--- src/components/Game/phasercore/GridControls.ts
+++ src/components/Game/phasercore/GridControls.ts
@@ -1,35 +1,24 @@
 import { Direction } from "./Direction";
+import { DirectionHandler } from "./DirectionHandler";
 import { GridPhysics } from "./GridPhysics";
 
-type WASDKeys = {
-  up: Phaser.Input.Keyboard.Key;
-  right: Phaser.Input.Keyboard.Key;
-  left: Phaser.Input.Keyboard.Key;
-  down: Phaser.Input.Keyboard.Key;
-};
-
 export class GridControls {
+  private directionController;
   constructor(
-    private input: Phaser.Input.InputPlugin,
+    input: Phaser.Input.InputPlugin,
     private gridPhysics: GridPhysics
-  ) {}
+  ) {
+    this.directionController = new DirectionHandler(input)
+  }
 
   update() {
-    const cursors = this.input.keyboard?.createCursorKeys();
-    const keys: WASDKeys | undefined = this.input.keyboard?.addKeys({
-      up: Phaser.Input.Keyboard.KeyCodes.W,
-      down: Phaser.Input.Keyboard.KeyCodes.S,
-      left: Phaser.Input.Keyboard.KeyCodes.A,
-      right: Phaser.Input.Keyboard.KeyCodes.D
-    }) as WASDKeys | undefined;
-    
-    if (cursors?.left.isDown || keys?.left.isDown) {
+    if (this.directionController.isLeft()) {
       this.gridPhysics.movePlayer(Direction.LEFT);
-    } else if (cursors?.right.isDown || keys?.right.isDown) {
+    } else if (this.directionController.isRight()) {
       this.gridPhysics.movePlayer(Direction.RIGHT);
-    } else if (cursors?.down.isDown || keys?.down.isDown) {
+    } else if (this.directionController.isDown()) {
       this.gridPhysics.movePlayer(Direction.DOWN);
-    } else if (cursors?.up.isDown || keys?.up.isDown) {
+    } else if (this.directionController.isUp()) {
       this.gridPhysics.movePlayer(Direction.UP);
     }
   }
