728f11d37546f6475b64b0b87c932f6ce7573808
toreis-up <toreis@reisan.dev>
2024-02-06 23:18:09 +0900

    :art: Feat: add my info

--- src/components/Game/Scene/AboutMeScene.ts
+++ src/components/Game/Scene/AboutMeScene.ts
@@ -1,6 +1,30 @@
 import EscMenuControls from '../phasercore/EscMenuControls'
 import { SceneBase } from './SceneBase'
 
+const today = new Date()
+const birthday = new Date(2005, 3 - 1, 18) // month is index
+
+const dateOptions: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+}
+
+const dialogText = `名前：とれいす（toreis）
+誕生日：${birthday.toLocaleDateString('ja-JP', dateOptions)}（${
+  today.getFullYear()
+  - birthday.getFullYear()
+  - (today.getMonth() <= birthday.getMonth()
+  && today.getDate() < birthday.getDate()
+    ? 1
+    : 0)
+}歳）
+
+学校：National Institute of Technology,
+　　　Kisarazu College
+所属：情報工学科 4年
+`
+
 export class AboutMeScene extends SceneBase {
   private menuControls!: EscMenuControls
   private aboutMeUiLayer!: Phaser.GameObjects.Container
@@ -58,8 +82,24 @@ export class AboutMeScene extends SceneBase {
       .setStrokeStyle(3, 0x907748, 1)
       .setFillStyle(0x454851, 0.9)
 
+    const dialogTextObject = this.add.text(
+      width / 2 + this.scene.scene.cameras.main.scrollX + 10,
+      height / 2
+        + this.scene.scene.cameras.main.scrollY
+        - (height / 12) * 5
+        + 10,
+      dialogText,
+      {
+        fontFamily: 'DotGothic16',
+        fontSize: '1.5rem',
+        lineSpacing: 16,
+        wordWrap: { width: width / 7 * 3 - 25 },
+      },
+    )
+
     this.aboutMeUiLayer.add(dialog)
     this.aboutMeUiLayer.add(playerSprite)
+    this.aboutMeUiLayer.add(dialogTextObject)
 
     playerSprite.anims.startAnimation('aboutMePlayerRotation')
   }
