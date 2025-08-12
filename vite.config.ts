d0997add74cb934ad991b9966b44a926ad2cbfa2
toreis-up <reisan03@reisan.dev>
2025-08-12 20:07:44 +0900

    :building_construction: Chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,43 +12,42 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls },
+      template: { transformAssetUrls, ...templateCompilerOptions },
       script: {
         propsDestructure: true, // https://zenn.dev/nanase_t/articles/f02f9499050112
       },
-      ...templateCompilerOptions,
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
       styles: {
-        configFile: 'src/styles/settings.scss',
+        configFile: "src/styles/settings.scss",
       },
     }),
     ViteFonts({
       google: {
         families: [
           {
-            name: 'Roboto',
-            styles: 'wght@100;300;400;500;700;900',
+            name: "Roboto",
+            styles: "wght@100;300;400;500;700;900",
           },
         ],
       },
     }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '#': fileURLToPath(new URL('./src/phaser', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "#": fileURLToPath(new URL("./src/phaser", import.meta.url)),
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
     port: 3000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     watch: {
       usePolling: true,
     },
   },
-})
+});
