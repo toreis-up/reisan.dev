8abb9858c5c0ff2349d4ae250b4e59526a623a57
toreis-up <reisan03@reisan.dev>
2026-01-25 22:19:23 +0900

    feat: preferred Dark mode

--- src/composables/useColor.ts
+++ src/composables/useColor.ts
@@ -1,3 +1,4 @@
+import { usePreferredDark } from '@vueuse/core'
 import chroma from 'chroma-js'
 import hello from 'hello-color'
 import { onMounted, ref } from 'vue'
@@ -9,23 +10,23 @@ export function useColor(safeColor = true) {
   function isSafe(hexString: string) {
     const [_, a, b] = chroma(hexString).lab()
     const C = Math.hypot(a, b)
-    return (C <= 70);
+    return (C <= 70)
   }
 
   function randomSafeLCh({
-  Lrange = [30, 80],
-  Crange = [0, 70],
-  huerange = [0, 360],
-} = {}) {
-  const L = Lrange[0] + Math.random() * (Lrange[1] - Lrange[0]);
-  const C = Crange[0] + Math.random() * (Crange[1] - Crange[0]);
-  const h = huerange[0] + Math.random() * (huerange[1] - huerange[0]);
-  return chroma.lch(L, C, h).hex();
-}
+    Lrange = [30, 80],
+    Crange = [0, 70],
+    huerange = [0, 360],
+  } = {}) {
+    const L = Lrange[0] + Math.random() * (Lrange[1] - Lrange[0])
+    const C = Crange[0] + Math.random() * (Crange[1] - Crange[0])
+    const h = huerange[0] + Math.random() * (huerange[1] - huerange[0])
+    return chroma.lch(L, C, h).hex()
+  }
 
   onMounted(() => {
-    const base = chroma(randomSafeLCh());
-    let result;
+    const base = chroma(randomSafeLCh())
+    let result
 
     do {
       result = hello(base.hex(), {
@@ -34,11 +35,20 @@ export function useColor(safeColor = true) {
         hues: 5,
       })
       if (!safeColor)
-        break;
+        break
     } while (!isSafe(result.color))
 
-    color.value = result.color
-    backgroundColor.value = result.base
+    const lightColor = chroma(result.color).luminance() < chroma(result.base).luminance() ? result.base : result.color
+    const darkColor = chroma(result.color).luminance() < chroma(result.base).luminance() ? result.color : result.base
+
+    if (usePreferredDark().value) {
+      color.value = lightColor
+      backgroundColor.value = darkColor
+    }
+    else {
+      color.value = lightColor
+      backgroundColor.value = darkColor
+    }
   })
 
   return { color, backgroundColor }
